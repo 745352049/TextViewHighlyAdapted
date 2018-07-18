ac076e2d2d07e13fff2aabb65ece96a28869ddd7
Baron_Nash <745352049@qq.com>
2018-07-18 15:39:17 +0800

    提交

--- TextViewHighlyAdapted/ViewController.m
+++ TextViewHighlyAdapted/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController ()<UITextViewDelegate>
+
+@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, assign) CGFloat keyH;
 
 @end
 
@@ -17,8 +23,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillHideNotification object:nil];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gesAction)];
+    tapGesture.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:tapGesture];
 }
-
+- (void)gesAction {
+    [self.textView resignFirstResponder];
+}
+#pragma mark - 键盘监听方法
+- (void)keyboardWillChangeFrame:(NSNotification *)notification {
+    NSDictionary *userInfo = notification.userInfo;
+    // 动画的持续时间
+    double duration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    // 键盘的frame
+    CGRect keyboardFrame = [userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    self.keyH = keyboardFrame.size.height;
+    [UIView animateWithDuration:duration animations:^{
+        if (keyboardFrame.origin.y == SCREEN_HEIGHT) {
+            self.textView.frame = CGRectMake(0, SCREEN_HEIGHT, SCREEN_WIDTH, 40);
+        } else {
+            self.textView.frame = CGRectMake(0, SCREEN_HEIGHT - keyboardFrame.size.height - 40, SCREEN_WIDTH, 40);
+        }
+    }];
+}
+- (void)textViewDidChange:(UITextView *)textView {
+    NSInteger height = ceilf([self.textView sizeThatFits:CGSizeMake(self.textView.bounds.size.width, MAXFLOAT)].height);
+    if (height != 40 ) {
+        self.textView.scrollEnabled = height > 80;
+        if (self.textView.scrollEnabled == NO) {
+            [self aaa:height];
+        }
+    }
+}
+- (void)aaa:(CGFloat)height {
+    self.textView.frame = CGRectMake(0, SCREEN_HEIGHT - self.keyH - height, SCREEN_WIDTH, height);
+}
+- (UITextView *)textView {
+    if (!_textView) {
+        _textView = [[UITextView alloc] init];
+        _textView.frame = CGRectMake(0, SCREEN_HEIGHT, SCREEN_WIDTH, 40);
+        _textView.textColor = [UIColor blackColor];
+        _textView.font = [UIFont systemFontOfSize:16];
+        _textView.delegate = self;
+        _textView.layer.borderWidth = 1.0f;
+        _textView.scrollEnabled = YES;
+    }
+    return _textView;
+}
+- (IBAction)buttonAction:(UIButton *)sender {
+    [self.view addSubview:self.textView];
+    [self.textView becomeFirstResponder];
+}
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
